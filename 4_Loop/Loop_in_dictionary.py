2581a672bcde96b3f34aa67925dc5b873f075514
SusmoyBarman1 <susmoy.barman.bd@gmail.com>
2019-02-28 13:38:30 +0600

    Loop in dictionary

---
+++ 4_Loop/Loop_in_dictionary.py
@@ -0,0 +1,9 @@
+
+student = {"Name":"Susmoy Barman","Age":23,"Dept.":"CSE","Courses:":["DSP","Database","Compiler "
+                                                                                       "Design"]}
+
+for key in student:
+    print(key)
+
+for key, value in student.items():
+    print(key,":",value)
